4fc7c423a82420991c66b6d1fa6ae4dc7acbf330
Beaskyy <afeez.akintobi@yahoo.com>
2026-06-26 10:30:24 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { PORT } from "./config/env.js";
+import userRoutes from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
+
+const app = express();
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
+app.get("/", (req, res) => {
+  res.send("Subscription Tracker");
+});
+
+app.listen(PORT, async () => {
+  console.log(`Server is listening on port http://localhost:${PORT}`);
+
+  await connectToDatabase();
+});
+
+export default app;
